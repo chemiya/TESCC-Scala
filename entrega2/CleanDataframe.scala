4618f1ae8c08a3078800189082acb13ccdf37e08
boni554 <148339644+boni554@users.noreply.github.com>
2023-11-05 19:54:43 +0100

    Update CleanDataframe.scala

--- entrega2/CleanDataframe.scala
+++ entrega2/CleanDataframe.scala
@@ -1,3 +1,15 @@
+/* Master en Ingeniería Informática - Universidad de Valladolid
+*
+*  TECNICAS ESCLABLES DE ANÁLISIS DE DATOS EN ENTORNOS BIG DATA: CLASIFICADORES
+*  Proyecto Software: Construcción y validación de un modelo de clasificación usando la metodología CRISP-DM y Spark
+*  
+*  Script para limpiar el DataFrame
+*
+*  Grupo 2: Sergio Agudelo Bernal
+*           Miguel Ángel Collado Alonso
+*           José María Lozano Olmedo.
+*/
+
 import org.apache.spark.sql.types.{IntegerType, StringType, DoubleType, StructField, StructType}
 import org.apache.spark.sql.{DataFrame, SparkSession,Row}
 import org.apache.spark.{SparkConf, SparkContext}
@@ -23,13 +35,11 @@ def cleanDataframe(census_df: DataFrame): DataFrame = {
     /*val eliminarColumnas = Seq("fill_inc_questionnaire_for_veterans_ad", "enrolled_in_edu_last_wk", "member_of_labor_union","reason_for_unemployment")
     val nuevoDataFrame = census_df.drop(eliminarColumnas: _*)
 
-
     nuevoDataFrame*/
 
     val nuevoDataFrame = census_df.select("class_of_worker","education","marital_status","major_industry_code","major_occupation_code","member_of_labor_union","race","sex","full_or_part_time_employment_status","hispanic_Origin","tax_filer_status","region_of_previous_residence","detailed_household_and_family_status","detailed_household_summary_in_house_instance_weight","migration_code_change_in_msa","migration_code_change_in_reg","migration_code_move_within_reg","live_in_this_house_one_year_ago","family_members_under_18","citizenship","income")
     nuevoDataFrame
 }
 }
-
 
 //"class_of_worker","industry_code","occupation_code","education","enrolled_in_edu_last_wk","marital_status","major_industry_code","major_occupation_code","member_of_labor_union","race","sex","full_or_part_time_employment_status","reason_for_unemployment","hispanic_Origin","tax_filer_status","region_of_previous_residence","state_of_previous_residence","detailed_household_and_family_status","detailed_household_summary_in_house_instance_weight","migration_code_change_in_msa","migration_code_change_in_reg","migration_code_move_within_reg","live_in_this_house_one_year_ago","migration_prev_res_in_sunbelt","family_members_under_18","country_of_birth_father","country_of_birth_mother","country_of_birth_self","citizenship","fill_inc_questionnaire_for_veterans_ad"
